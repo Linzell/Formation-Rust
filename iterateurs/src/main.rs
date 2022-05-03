1a78ccabdba22130b43406415ded8eb96d77eed5
Charlie Cohen <linzellart@gmail.com>
2022-05-03 17:44:26 +0200

    Update

--- iterateurs/src/main.rs
+++ iterateurs/src/main.rs
@@ -1,31 +1,77 @@
 use std::thread;
 use std::time::Duration;
 
-fn generer_exercices(intensite: u32, nombre_aleatoire: u32) {
-  let fermeture_lente = |nombre| {
-      println!("calcul très lent ...");
-      thread::sleep(Duration::from_secs(2));
-      nombre
-  };
+struct Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calcul: T,
+    valeur: Option<u32>,
+}
 
-  if intensite < 25 {
-      println!("Aujourd'hui, faire {} pompes !", fermeture_lente(intensite));
-      println!("Ensuite, faire {} abdominaux !", fermeture_lente(intensite));
-  } else {
-      if nombre_aleatoire == 3 {
-          println!("Faites une pause aujourd'hui ! Rappelez-vous de bien vous hydrater !");
-      } else {
-          println!(
-              "Aujourd'hui, courrez pendant {} minutes !",
-              fermeture_lente(intensite)
-          );
-      }
-  }
+impl<T> Cache<T>
+where
+    T: Fn(u32) -> u32
+{
+    fn new(calcul: T) -> Cache<T> {
+        Cache {
+            calcul,
+            valeur: None,
+        }
+    }
+
+    fn valeur(&mut self, arg: u32) -> u32 {
+        match self.valeur {
+            Some(v) => v,
+            None => {
+                let v = (self.calcul)(arg);
+                self.valeur = Some(v);
+                v
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn appel_avec_differentes_valeurs() {
+        let mut c = Cache::new(|a| a);
+
+        let v1 = c.valeur(1);
+        let v2 = c.valeur(2);
+
+        assert_eq!(v2, 2);
+    }
+}
+
+fn generer_exercices(intensite: u32, nombre_aleatoire: u32) {
+    let mut resultat_lent = Cache::new(|nombre| {
+        println!("calcul très lent ...");
+        thread::sleep(Duration::from_secs(2));
+        nombre
+    });
+
+    if intensite < 25 {
+        println!("Aujourd'hui, faire {} pompes !", resultat_lent.valeur(intensite));
+        println!("Ensuite, faire {} abdominaux !", resultat_lent.valeur(intensite));
+    } else {
+        if nombre_aleatoire == 3 {
+            println!("Faites une pause aujourd'hui ! Rappelez-vous de bien vous hydrater !");
+        } else {
+            println!(
+                "Aujourd'hui, courrez pendant {} minutes !",
+                resultat_lent.valeur(intensite)
+            );
+        }
+    }
 }
 
 fn main() {
-  let valeur_utilisateur_simule = 10;
-  let nombre_aleatoire_simule = 7;
+    let valeur_utilisateur_simule = 10;
+    let nombre_aleatoire_simule = 7;
 
-  generer_exercices(valeur_utilisateur_simule, nombre_aleatoire_simule);
+    generer_exercices(valeur_utilisateur_simule, nombre_aleatoire_simule);
 }
