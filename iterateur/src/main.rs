f33e3e7c23ef13dd558fa01c330161480993b8a6
Charlie Cohen <linzellart@gmail.com>
2022-05-04 19:36:40 +0200

    Update

--- iterateur/src/main.rs
+++ iterateur/src/main.rs
@@ -19,6 +19,29 @@ fn chaussures_a_la_pointure(chaussures: Vec<Chaussure>, pointure_chaussure: u32)
     .collect()
 }
 
+struct Compteur {
+  compteur: u32,
+}
+
+impl Compteur {
+  fn new() -> Compteur {
+    Compteur { compteur: 0 }
+  }
+}
+
+impl Iterator for Compteur {
+  type Item = u32;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.compteur < 5 {
+      self.compteur += 1;
+      Some(self.compteur)
+    } else {
+      None
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -85,21 +108,24 @@ mod tests {
       ]
     );
   }
-}
+  #[test]
+  fn appel_direct_a_next() {
+    let mut compteur = Compteur::new();
 
-struct Compteur {
-  compteur: u32,
-}
-
-impl Iterator for Compteur {
-  type Item = u32;
-
-  fn next(&mut self) -> Option<Self::Item> {
-      if self.compteur < 5 {
-          self.compteur += 1;
-          Some(self.compteur)
-      } else {
-          None
-      }
+    assert_eq!(compteur.next(), Some(1));
+    assert_eq!(compteur.next(), Some(2));
+    assert_eq!(compteur.next(), Some(3));
+    assert_eq!(compteur.next(), Some(4));
+    assert_eq!(compteur.next(), Some(5));
+    assert_eq!(compteur.next(), None);
+  }
+  #[test]
+  fn utilisation_des_autres_methodes_du_trait_iterator() {
+    let somme: u32 = Compteur::new()
+      .zip(Compteur::new().skip(1))
+      .map(|(a, b)| a * b)
+      .filter(|x| x % 3 == 0)
+      .sum();
+    assert_eq!(18, somme);
   }
 }
