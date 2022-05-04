507b74711acc72237b39ead9ab0eccb0fc199f75
Charlie Cohen <linzellart@gmail.com>
2022-05-04 18:19:18 +0200

    Update

--- iterateur/src/main.rs
+++ iterateur/src/main.rs
@@ -86,3 +86,20 @@ mod tests {
     );
   }
 }
+
+struct Compteur {
+  compteur: u32,
+}
+
+impl Iterator for Compteur {
+  type Item = u32;
+
+  fn next(&mut self) -> Option<Self::Item> {
+      if self.compteur < 5 {
+          self.compteur += 1;
+          Some(self.compteur)
+      } else {
+          None
+      }
+  }
+}
