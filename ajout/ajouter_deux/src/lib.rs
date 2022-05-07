20f6ee5359cd7ff5e60a0e32a928f3fd60738b13
Charlie Cohen <linzellart@gmail.com>
2022-05-07 23:01:30 +0200

    Update

--- ajout/ajouter_deux/src/lib.rs
+++ ajout/ajouter_deux/src/lib.rs
@@ -10,4 +10,9 @@ mod tests {
     fn cela_fonctionne() {
         assert_eq!(4, ajouter_deux(2));
     }
+
+    #[test]
+    fn cela_fonctionne_aussi() {
+        assert_eq!(6, ajouter_deux(4));
+    }
 }
