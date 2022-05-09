e6a31e9b28d5b5baf44780b2211de7bde359b2ce
Charlie Cohen <linzellart@gmail.com>
2022-05-09 15:47:06 +0200

    update

--- drop/src/main.rs
+++ drop/src/main.rs
@@ -4,16 +4,21 @@ struct PointeurPerso {
 
 impl Drop for PointeurPerso {
   fn drop(&mut self) {
-      println!("Nettoyage d'un PointeurPerso avec la donnée `{}` !", self.donnee);
+    println!(
+      "Nettoyage d'un PointeurPerso avec la donnée `{}` !",
+      self.donnee
+    );
   }
 }
 
 fn main() {
   let c = PointeurPerso {
-      donnee: String::from("des trucs"),
+    donnee: String::from("des trucs"),
   };
+  println!("PointeurPerso créé.");
+  drop(c);
   let d = PointeurPerso {
-      donnee: String::from("d'autres trucs"),
+    donnee: String::from("d'autres trucs"),
   };
-  println!("PointeurPersos créés.");
+  println!("PointeurPerso libéré avant la fin du main.");
 }
