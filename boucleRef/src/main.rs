0aa4c992968ef3e5e4e78792ef310e9a7837bbff
Charlie Cohen <linzellart@gmail.com>
2022-05-11 16:53:18 +0200

    Update

--- boucleRef/src/main.rs
+++ boucleRef/src/main.rs
@@ -17,4 +17,26 @@ impl List {
     }
 }
 
-fn main() {}
+fn main() {
+  let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+  println!("compteur initial de a = {}", Rc::strong_count(&a));
+  println!("prochain élément de a = {:?}", a.parcourir());
+
+  let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+  println!("compteur de a après création de b = {}", Rc::strong_count(&a));
+  println!("compteur initial de b = {}", Rc::strong_count(&b));
+  println!("prochain élément de b = {:?}", b.parcourir());
+
+  if let Some(lien) = a.parcourir() {
+      *lien.borrow_mut() = Rc::clone(&b);
+  }
+
+  println!("compteur de b après avoir changé a = {}", Rc::strong_count(&b));
+  println!("compteur de a après avoir changé a = {}", Rc::strong_count(&a));
+
+  // Décommentez la ligne suivante pour constater que nous sommes dans
+  // une boucle de références, cela fera déborder la pile
+  // println!("prochain élément de a = {:?}", a.parcourir());
+}
