92fa7a6f51626661090d8d4d1cb2febc93d4d86e
Charlie Cohen <linzellart@gmail.com>
2022-05-12 15:38:33 +0200

    Update

--- conc_mess/src/main.rs
+++ conc_mess/src/main.rs
@@ -1,14 +1,25 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let valeur = String::from("salut");
-        tx.send(valeur).unwrap();
+        let valeurs = vec![
+            String::from("salutations"),
+            String::from("à partir"),
+            String::from("de la"),
+            String::from("nouvelle tâche"),
+        ];
+
+        for valeur in valeurs {
+            tx.send(valeur).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let recu = rx.recv().unwrap();
-    println!("On a reçu : {}", recu);
+    for recu in rx {
+        println!("On a reçu : {}", recu);
+    }
 }
