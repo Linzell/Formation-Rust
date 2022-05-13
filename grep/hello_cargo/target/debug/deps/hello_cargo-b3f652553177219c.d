f3b77c61a749f4bb6ebf4291b2649b168edb3d16
Charlie Cohen <linzellart@gmail.com>
2022-05-13 14:16:05 +0200

    Update

---
+++ grep/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d
@@ -0,0 +1,5 @@
+/home/linzell/projects/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.rmeta: src/main.rs
+
+/home/linzell/projects/hello_cargo/target/debug/deps/hello_cargo-b3f652553177219c.d: src/main.rs
+
+src/main.rs:
---
+++ grep/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/linzell/projects/hello_cargo/target/debug/hello_cargo: /home/linzell/projects/hello_cargo/src/main.rs
