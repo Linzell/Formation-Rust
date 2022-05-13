cf999ff5b3272001e61bddccd4a4b8a11173f5ca
Charlie Cohen <linzellart@gmail.com>
2022-05-13 16:13:37 +0200

    update

--- poo/libs/src/lib.rs
+++ poo/libs/src/lib.rs
@@ -1,8 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+pub struct CollectionMoyennee {
+  liste: Vec<i32>,
+  moyenne: f64,
+}
+
+impl CollectionMoyennee {
+  pub fn ajouter(&mut self, valeur: i32) {
+      self.liste.push(valeur);
+      self.mettre_a_jour_moyenne();
+  }
+
+  pub fn retirer(&mut self) -> Option<i32> {
+      let resultat = self.liste.pop();
+      match resultat {
+          Some(valeur) => {
+              self.mettre_a_jour_moyenne();
+              Some(valeur)
+          }
+          None => None,
+      }
+  }
+
+  pub fn moyenne(&self) -> f64 {
+      self.moyenne
+  }
+
+  fn mettre_a_jour_moyenne(&mut self) {
+      let total: i32 = self.liste.iter().sum();
+      self.moyenne = total as f64 / self.liste.len() as f64;
+  }
 }
