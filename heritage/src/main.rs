cb3287c69a6c66a59b79b605ae0cbbb3935e93f1
Charlie Cohen <linzellart@gmail.com>
2022-05-13 23:18:28 +0200

    Update

--- heritage/src/main.rs
+++ heritage/src/main.rs
@@ -1,3 +1,45 @@
+use libs::Affichable;
+
+struct ListeDeroulante {
+  largeur: u32,
+  hauteur: u32,
+  options: Vec<String>,
+}
+
+impl Affichable for ListeDeroulante {
+  fn afficher(&self) {
+    println!(
+      "Liste déroulante de taille {}x{}",
+      self.largeur, self.hauteur
+    );
+
+    for option in self.options.iter() {
+      println!("{}", option);
+    }
+  }
+}
+
+use libs::{Bouton, Ecran};
+
 fn main() {
-    println!("Hello, world!");
+  let ecran = Ecran {
+    composants: vec![
+      Box::new(ListeDeroulante {
+        largeur: 75,
+        hauteur: 10,
+        options: vec![
+          String::from("Oui"),
+          String::from("Peut-être"),
+          String::from("Non"),
+        ],
+      }),
+      Box::new(Bouton {
+        largeur: 50,
+        hauteur: 10,
+        libelle: String::from("OK"),
+      }),
+    ],
+  };
+
+  ecran.executer();
 }
