1aac65da01def8c7e9a1cc0e2d14b56ae640d9e6
Charlie Cohen <linzellart@gmail.com>
2022-05-17 13:56:18 +0200

    Update

--- trait_advanced/src/main.rs
+++ trait_advanced/src/main.rs
@@ -27,7 +27,6 @@ fn mesure (premier: Point, deuxieme: Point, resultat: Point) {
 fn main() {
 
   mesure(Point{x: 2, y: 0}, Point{x: 1, y: 3}, Point{x: 3, y: 3});
-
   mesure(Point{x: 5, y: 2}, Point{x: 0, y: 11}, Point{x: 5, y: 13});
 
 }
