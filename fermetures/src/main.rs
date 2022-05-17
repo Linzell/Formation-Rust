ae329c1a87a51e79c4a78b61de1f2c46ba8c84af
Charlie Cohen <linzellart@gmail.com>
2022-05-17 17:48:12 +0200

    up

--- fermetures/src/main.rs
+++ fermetures/src/main.rs
@@ -7,7 +7,19 @@ fn le_faire_deux_fois(f: fn(i32) -> i32, arg: i32) -> i32 {
 }
 
 fn main() {
+  
   let reponse = le_faire_deux_fois(ajouter_un, 5);
 
   println!("La réponse est : {}", reponse);
+
+  let liste_de_nombres = vec![1, 2, 3];
+  let liste_de_chaines: Vec<String> = liste_de_nombres.iter().map(|i| i.to_string()).collect();
+
+  println!("La liste de chaines est : {:?}", liste_de_chaines);
+
+  let liste_de_nombres = vec![4, 5, 6];
+  let liste_de_chaines: Vec<String> = liste_de_nombres.iter().map(ToString::to_string).collect();
+
+  println!("La liste de chaines est : {:?}", liste_de_chaines);
+
 }
