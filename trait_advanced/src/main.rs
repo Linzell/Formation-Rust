d805a87ce1b006078c1d24f7a89c76c15a50aef3
Charlie Cohen <linzellart@gmail.com>
2022-05-17 13:52:33 +0200

    Up

--- trait_advanced/src/main.rs
+++ trait_advanced/src/main.rs
@@ -25,16 +25,10 @@ fn mesure (premier: Point, deuxieme: Point, resultat: Point) {
 }
 
 fn main() {
-  mesure(Point{x: 2, y: 0}, Point{x: 1, y: 3}, Point{x: 3, y: 3});
-}
 
-#[test]
-fn trois() {
   mesure(Point{x: 2, y: 0}, Point{x: 1, y: 3}, Point{x: 3, y: 3});
-}
 
-#[test]
-fn quatre() {
-  mesure(Point{x: 2, y: 0}, Point{x: 2, y: 2}, Point{x: 4, y: 2});
+  mesure(Point{x: 5, y: 2}, Point{x: 0, y: 11}, Point{x: 5, y: 13});
+
 }
 
