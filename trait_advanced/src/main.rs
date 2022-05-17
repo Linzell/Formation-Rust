a9cf4cd11f972906275eb27c43f102a8e747b63c
Charlie Cohen <linzellart@gmail.com>
2022-05-17 13:46:46 +0200

    Update

--- trait_advanced/src/main.rs
+++ trait_advanced/src/main.rs
@@ -1,13 +1,40 @@
-pub trait Iterator {
-  type Item;
+use std::ops::Add;
 
-  fn next(&mut self) -> Option<Self::Item>;
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+  x: i32,
+  y: i32,
+}
+
+impl Add for Point {
+  type Output = Point;
+
+  fn add(self, other: Point) -> Point {
+    Point {
+      x: self.x + other.x,
+      y: self.y + other.y,
+    }
+  }
+}
+
+fn mesure (premier: Point, deuxieme: Point, resultat: Point) {
+  assert_eq!(
+    premier + deuxieme,
+    resultat
+  );
 }
 
 fn main() {
-  let mut iter = (1..10).into_iter();
-
-  while let Some(x) = iter.next() {
-    println!("{}", x);
-  }
+  mesure(Point{x: 2, y: 0}, Point{x: 1, y: 3}, Point{x: 3, y: 3});
 }
+
+#[test]
+fn trois() {
+  mesure(Point{x: 2, y: 0}, Point{x: 1, y: 3}, Point{x: 3, y: 3});
+}
+
+#[test]
+fn quatre() {
+  mesure(Point{x: 2, y: 0}, Point{x: 2, y: 2}, Point{x: 4, y: 2});
+}
+
