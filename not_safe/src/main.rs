12462c0de52ae4fa03d7b3a7dbcf31587ced6217
Charlie Cohen <linzellart@gmail.com>
2022-05-17 10:21:59 +0200

    Up

--- not_safe/src/main.rs
+++ not_safe/src/main.rs
@@ -1,3 +1,19 @@
+use std::slice;
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
   let mut nombre = 5;
 
@@ -13,4 +29,19 @@ fn main() {
     println!("{}", *r2);
     dangereux();
   }
+
+  let mut v = vec![1, 2, 3, 4, 5, 6];
+
+  let r = &mut v[..];
+
+  let (a, b) = r.split_at_mut(3);
+
+  assert_eq!(a, &mut [1, 2, 3]);
+  assert_eq!(b, &mut [4, 5, 6]);
+
+  let mut vector = vec![1, 2, 3, 4, 5, 6];
+  let (left, right) = split_at_mut(&mut vector, 3);
+
+  assert_eq!(left, &mut [1, 2, 3]);
+  assert_eq!(right, &mut [4, 5, 6]);
 }
